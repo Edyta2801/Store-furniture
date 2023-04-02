f4d32792a8a0de6aa8965509fa5506c071443769
BTN5211\edyta.szarowska <edyta.szarowska@interia.pl>
2023-04-02 13:05:23 +0200

    add auth

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { useProductsContext } from '../context/products_context'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useProductsContext } from "../context/products_context";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
 const CartButton = () => {
-  const { closeSidebar } = useProductsContext()
-  const { total_items, clearCart } = useCartContext()
-  const { loginWithRedirect, myUser, logout } = useUserContext()
+  const { closeSidebar } = useProductsContext();
+  const { total_items, clearCart } = useCartContext();
+  const { loginWithRedirect, myUser, logout } = useUserContext();
   return (
-    <Wrapper className='cart-btn-wrapper'>
-      <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
+    <Wrapper className="cart-btn-wrapper">
+      <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
-        <span className='cart-container'>
+        <span className="cart-container">
           <FaShoppingCart />
-          <span className='cart-value'>{total_items}</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       {myUser ? (
         <button
-          type='button'
-          className='auth-btn'
+          type="button"
+          className="auth-btn"
           onClick={() => {
-            clearCart()
-            localStorage.removeItem('user')
-            logout({ returnTo: window.location.origin })
+            clearCart();
+            localStorage.removeItem("user");
+            logout({ returnTo: window.location.origin });
           }}
         >
-          Logout <FaUserMinus />
+           <p style={{fontSize:'small', margin:'0 20px 0 30px'}}>Hello,{myUser.name}</p>
+           Logout <FaUserMinus />
         </button>
       ) : (
-        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+        <button type="button" className="auth-btn" onClick={loginWithRedirect}>
           Login <FaUserPlus />
         </button>
       )}
+    
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -89,7 +91,8 @@ const Wrapper = styled.div`
     letter-spacing: var(--spacing);
     svg {
       margin-left: 5px;
+      min-width:24px;
     }
   }
-`
-export default CartButton
+`;
+export default CartButton;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ProductsProvider } from './context/products_context';
-import { FilterProvider } from './context/filter_context';
-import { CartProvider } from './context/cart_context';
-import { UserProvider } from './context/user_context';
-import { Auth0Provider } from '@auth0/auth0-react';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+// domain={process.env.REACT_APP_AUTH_DOMAIN}
+// clientId={process.env.REACT_APP_CLIENT_ID}
+
 
 root.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_CLIENT_ID}
+    domain="dev-im1wj6ecuropzjst.us.auth0.com"
+    clientId="VOmqtRXYVAMyDWC1M8cMfVNo4Gb293uy"
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
+    cacheLocation="localstorage"
   >
     <UserProvider>
       <ProductsProvider>
